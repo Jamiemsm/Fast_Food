f6370882a87c65ce1fc73847f4f0f5341a36eaf0
Jamiemsm <msmjamie@gmail.com>
2019-02-05 10:12:33 +0100

    Created Order Class

---
+++ Fast_Food/Fast_Food/Order.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fast_Food
+{
+	class Order
+	{
+		private Product product;
+
+		public Product Product
+		{
+			get { return product; }
+			set { product = value; }
+		}
+
+
+		private int price;
+
+		public int Price
+		{
+			get { return price; }
+			set { price = value; }
+		}
+
+		public Order()
+		{
+			Product product = new Product();
+		}
+	}
+}
